8503bc94dcf78c56adb9da5bc9d3a029f7597e5e
wuyu <wuyu@yunify.com>
2022-07-21 13:58:31 +0800

    Add pkg

---
+++ pkg/pgkgenerator/cmd.go
@@ -0,0 +1,46 @@
+package pgkgenerator
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+func Do(plugin Plugin) {
+	if plugin.Name() == "" {
+		panic("plugin name not sets.")
+	}
+	if plugin.Version() == "" {
+		panic("plugin version not sets.")
+	}
+
+	if len(os.Args) == 2 && (os.Args[1] == "version" || os.Args[1] == "--version") {
+		_, _ = fmt.Fprintf(os.Stdout, "%v %v\n", filepath.Base(os.Args[0]), plugin.Version())
+		os.Exit(0)
+	}
+
+	var options = protogen.Options{
+		ParamFunc: plugin.ParamFunc(),
+		ImportRewriteFunc: func(importPath protogen.GoImportPath) protogen.GoImportPath {
+			return importPath
+		},
+	}
+
+	options.Run(func(pp *protogen.Plugin) error {
+		for _, file := range pp.Files {
+			if !file.Generate {
+				continue
+			}
+			if !plugin.Init(file) {
+				continue
+			}
+
+			_ = generateFile(plugin, pp, file)
+		}
+		pp.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		return nil
+	})
+}
---
+++ pkg/pgkutils/field.go
@@ -0,0 +1,133 @@
+package pgkutils
+
+import (
+	"fmt"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+// FieldGoType returns the Go type used for a field.
+func FieldGoType(g *protogen.GeneratedFile, field *protogen.Field) (goType string) {
+	if field.Desc.IsWeak() {
+		//return "struct{}", false
+		panic(fmt.Sprintf("unsupported case IsWeak; field: %s", field.Desc.FullName()))
+	}
+
+	switch field.Desc.Kind() {
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+		goType = "int32"
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+		goType = "uint32"
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+		goType = "int64"
+	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		goType = "uint64"
+	case protoreflect.FloatKind:
+		goType = "float32"
+	case protoreflect.DoubleKind:
+		goType = "float64"
+	case protoreflect.StringKind:
+		goType = "string"
+	case protoreflect.BytesKind:
+		goType = "[]byte"
+	case protoreflect.BoolKind:
+		goType = "bool"
+	case protoreflect.EnumKind:
+		goType = g.QualifiedGoIdent(field.Enum.GoIdent)
+	case protoreflect.MessageKind, protoreflect.GroupKind:
+		goType = "*" + g.QualifiedGoIdent(field.Message.GoIdent)
+	}
+
+	switch {
+	case field.Desc.IsList():
+		return "[]" + goType
+	case field.Desc.IsMap():
+		keyType := FieldGoType(g, field.Message.Fields[0])
+		valType := FieldGoType(g, field.Message.Fields[1])
+		return fmt.Sprintf("map[%v]%v", keyType, valType)
+	}
+	return goType
+}
+
+// FieldIsOneOf check the field is oneof
+func FieldIsOneOf(field *protogen.Field) bool {
+	return field.Oneof != nil && !field.Oneof.Desc.IsSynthetic()
+}
+
+// FieldIsPointer check the field is pointer
+func FieldIsPointer(field *protogen.Field) bool {
+	if field.Desc.IsMap() {
+		return false
+	}
+	if field.Desc.IsList() {
+		return false
+	}
+	if FieldIsOneOf(field) {
+		return false
+	}
+	switch field.Desc.Kind() {
+	case protoreflect.BytesKind:
+		// rely on nullability of slices for presence
+		return false
+	case protoreflect.MessageKind, protoreflect.GroupKind:
+		// rely on nullability of slices for presence
+		return false
+	}
+	return field.Desc.HasPresence()
+}
+
+// FieldContainMessage check whether the field contains data of type `message`.
+func FieldContainMessage(field *protogen.Field) bool {
+	if field.Desc.IsList() && field.Desc.Kind() == protoreflect.MessageKind {
+		return true
+	}
+	if field.Desc.IsMap() && field.Desc.MapValue().Kind() == protoreflect.MessageKind {
+		return true
+	}
+	if !field.Desc.IsMap() && field.Desc.Kind() == protoreflect.MessageKind {
+		return true
+	}
+	if FieldIsOneOf(field) && field.Desc.Kind() == protoreflect.MessageKind {
+		return true
+	}
+	return false
+}
+
+// FieldProtoName returns the field name that you declares in proto file.
+func FieldProtoName(field *protogen.Field, inOneOf bool) string {
+	var name string
+
+	isOneOf := FieldIsOneOf(field)
+	if isOneOf && !inOneOf {
+		name = string(field.Oneof.Desc.Name())
+	} else {
+		name = string(field.Desc.Name())
+	}
+	return name
+}
+
+// FieldGoName returns the field name in Golang.
+func FieldGoName(field *protogen.Field, inOneOf bool) string {
+	var name string
+
+	isOneOf := FieldIsOneOf(field)
+	if isOneOf && !inOneOf {
+		name = field.Oneof.GoName
+	} else {
+		name = field.GoName
+	}
+	return name
+}
+
+// LoadFieldList return valid fields list in message.
+func LoadFieldList(message *protogen.Message) []*protogen.Field {
+	fields := make([]*protogen.Field, 0)
+	for _, field := range message.Fields {
+		if FieldIsOneOf(field) && field.Oneof.Fields[0] != field {
+			continue // only generate for first appearance
+		}
+		fields = append(fields, field)
+	}
+	return fields
+}
