9b903a88c5df04b8da52b70c78757d4c97569fca
wuyu <wuyu@yunify.com>
2023-05-01 16:03:53 +0800

    Add package pgkerror

---
+++ pgkerror/error.go
@@ -0,0 +1,20 @@
+package pgkerror
+
+import "fmt"
+
+type Error struct {
+	reason string
+}
+
+func (e *Error) Error() string {
+	return e.reason
+}
+
+func New(format string, a ...interface{}) *Error {
+	return &Error{reason: fmt.Sprintf(format, a...)}
+}
+
+func Panic(format string, a ...interface{}) {
+	err := &Error{reason: fmt.Sprintf(format, a...)}
+	panic(err)
+}
